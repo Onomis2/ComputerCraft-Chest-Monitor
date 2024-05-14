7ab1dc75f33ad996840e64b38db8abf67a3ee824
Simon <onomis2@outlook.com>
2024-05-14 15:42:53 +0200

    Added files ready for testing

---
+++ chest_monitor.lua
@@ -0,0 +1,36 @@
+local monitor = peripheral.find("monitor")
+local chest = peripheral.find("inventory")
+local center = (monitor.getSize() / 2)
+print(center)
+function DisplayChest(chest, monitor)
+    local parse = 1
+    local centerItem = 0
+    local centerCount = 0
+    
+    for slot, item in pairs(chest.list()) do
+        local _, colonIndex = string.find(item.name, ":")
+        local itemName = item.name
+        if colonIndex then
+            itemName = string.sub(item.name, colonIndex + 1)
+        end
+        itemName = itemName:gsub("_", " ")
+        centerItem = center - (string.len(itemName) / 2)
+        centerCount = center - (string.len(item.count) / 2) - 1
+        monitor.setCursorPos(centerItem,parse)
+        monitor.setTextColor(0)
+        monitor.write(itemName)
+        parse = parse + 1
+        monitor.setCursorPos(centerCount,parse)
+        monitor.setTextColor(64)
+        monitor.write("x "..item.count)
+        parse = parse + 1
+    end
+end
+
+while chest ~= nil do
+    monitor.clear()
+    monitor.setBackgroundColor(32)
+    monitor.setCursorPos(1,1)
+    DisplayChest(chest, monitor)
+    sleep(1)
+end
---
+++ install.lua
@@ -0,0 +1,20 @@
+response = http.get("https://github.com/Onomis2/ComputerCraft-Chest-Monitor/blob/main/install.lua")
+
+if response then
+    local content = response.readAll()
+    response.close()
+    local file = fs.open("chest_monitor.lua", "w")
+    file.write(content)
+    file.close()
+
+    local startupFile = fs.open("startup.lua", "w")
+    startupFile.write("shell.run('chest_monitor.lua')")
+    startupFile.close()
+
+    print("Succesfully downloaded files. Rebooting PC in 3 seconds...")
+    sleep(3)
+
+    shell.run("reboot")
+else
+    print("There was an error trying to fetch the github repository.")
+end
